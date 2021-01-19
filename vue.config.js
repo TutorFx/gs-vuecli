b5c8a46dcc82eec432ef327b3ef4fc4ae1187276
TutorFx <gabriel_serejo@live.com>
2021-01-19 11:43:08 -0300

    PWA update

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+// vue.config.js
+module.exports = {
+    pwa: {
+        name: "ITH Pós Graduação",
+        short_name: "ITH Pós Graduação",
+        appleMobileWebAppCapable: true,
+        themeColor: "#191A35",
+        msTileColor: "#191A35",
+        manifestOptions: {
+          background_color: "#FFF"
+        },
+        start_url: "./index.html",
+        display: "standalone",
+      },
+      //devServer: {
+      //  https: true
+      //}
+  }
